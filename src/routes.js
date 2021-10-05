a34e47b1ec1de2206bb32611325a63618de2a130
홍성현 <hongseonghyeon@hongseonghyeons-MacBook-Air.local>
2021-10-05 15:27:31 +0900

    1005 merge after dump

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Schedule from './views/pages/mainPage/'
+import Album from './views/pages/album/album'
+import Schedule from './views/pages/mainPage/Schedule'
 import Daytimedev from './views/pages/mainPage/Daytimedev'
 import Stat from './views/pages/mainPage/Daytimedev'
 const SidebarClendar = React.lazy(() => import('./views/pages/navCalendar/SidebarCalendar') )
@@ -7,9 +8,11 @@ const SidebarClendar = React.lazy(() => import('./views/pages/navCalendar/Sideba
 
 const routes = [
     { path: '/', exact: true, name: 'Home' },
+    {path: '/Album', exact: true, name: 'Album', component: Album},
     { path: '/main', exact: true, name: 'Main', component: Schedule },
     { path: '/dev', exact: true, name: 'DEV', component: Daytimedev },
-    { path: '/dev', exact: true, name: 'DEV', component: Daytimedev }
+    { path: '/dev', exact: true, name: 'DEV', component: Daytimedev },
+    
 
 ];
 export default routes;
---
+++ src/views/pages/album/album.js
@@ -0,0 +1,85 @@
+import React,{Component} from 'react'
+import ImageGallery from 'react-image-gallery'
+
+const images1 = [
+    {
+        original: 'https://picsum.photos/id/1018/1000/600/',
+        thumbnail: 'https://picsum.photos/id/1018/250/150/',
+    },
+    {
+        original: 'https://picsum.photos/id/1015/1000/600/',
+        thumbnail: 'https://picsum.photos/id/1015/250/150/',
+    },
+    {
+        original: 'https://picsum.photos/id/1019/1000/600/',
+        thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    },
+    {
+        original: 'https://picsum.photos/id/1019/1000/600/',
+        thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    },
+    {
+        original: 'https://picsum.photos/id/1019/1000/600/',
+        thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    },
+    {
+        original: 'https://picsum.photos/id/1019/1000/600/',
+        thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    },
+    {
+        original: 'https://picsum.photos/id/1019/1000/600/',
+        thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    },
+
+    
+  ];
+
+  const images2 = [
+
+    {
+        original: 'https://picsum.photos/id/1015/1000/600/',
+        thumbnail: 'https://picsum.photos/id/1015/250/150/',
+    },
+    {
+        original: 'https://picsum.photos/id/1019/1000/600/',
+        thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    },
+    {
+        original: 'https://picsum.photos/id/1019/1000/600/',
+        thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    },
+    {
+        original: 'https://picsum.photos/id/1019/1000/600/',
+        thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    },
+    {
+        original: 'https://picsum.photos/id/1019/1000/600/',
+        thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    },
+    {
+        original: 'https://picsum.photos/id/1019/1000/600/',
+        thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    },
+
+    
+  ];
+
+class Album extends Component{
+
+    render(){
+        return(
+            <div>
+                    <h1>running</h1>
+                <ImageGallery items={images1}/>
+                    <h1>Dancing</h1>
+                <ImageGallery items={images2}/>
+                    <h1>Soccer</h1>
+                <ImageGallery items={images1}/>
+                    <h1>Climbing</h1>
+                <ImageGallery items={images2}/>
+            </div>
+        )
+    }
+}
+
+export default Album
