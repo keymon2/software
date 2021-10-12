ea6ca790fc07ae0884204663862dffd939da401a
홍성현 <hongseonghyeon@hongseonghyeons-MacBook-Air.local>
2021-10-12 12:09:28 +0900

    chat

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Schedule from './views/pages/mainPage/Schedule'
 import Daytimedev from './views/pages/mainPage/Daytimedev'
 import Album from './views/pages/album/album'
+import Chat from './views/pages/chatting/chat'
 import Stat from './views/pages/mainPage/Daytimedev'
 const SidebarClendar = React.lazy(() => import('./views/pages/navCalendar/SidebarCalendar') )
 
@@ -10,6 +11,7 @@ const SidebarClendar = React.lazy(() => import('./views/pages/navCalendar/Sideba
 const routes = [
     { path: '/', exact: true, name: 'Home' },
     {path: '/Album', exact: true, name: 'Album', component: Album},
+    {path: '/Chat', exact: true, name: 'Chat', component: Chat},
     { path: '/main', exact: true, name: 'Main', component: Schedule },
     { path: '/dev', exact: true, name: 'DEV', component: Daytimedev },
     { path: '/dev', exact: true, name: 'DEV', component: Daytimedev },
